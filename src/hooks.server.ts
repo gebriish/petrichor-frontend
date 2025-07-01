6d69de612007f531948710d70c07c64c9866b2f4
gebriish <gaureeshjr@gmail.com>
2025-07-01 21:24:50 +0530

    feat: filter route requests to aboutus and caportal

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,37 @@
 import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 
+// (gebriish) : allowed routes go here
+const frontendRoutes = [
+	'/',
+	'/aboutus',
+	'/caportal',
+]
+
+
+const requestFilter : Handle = async ({event, resolve}) => {
+	const path = event.url.pathname.toLowerCase();
+
+	const isAllowed = frontendRoutes.some(route =>
+		path === route || path.startsWith(route + '/')
+	);
+
+	if (!isAllowed) {
+		return new Response('Not Found', { status: 404 });
+	}
+
+	return resolve(event);
+}
+
 const handleParaglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request, locale }) => {
 		event.request = request;
 
-		return resolve(event, {
-			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+		return requestFilter({ event, resolve: (_event) =>
+			resolve(_event, {
+				transformPageChunk: ({ html }) =>
+					html.replace('%paraglide.lang%', locale)
+			})
 		});
 	});
 
